dee594f8d758523dab04742dc99d96c26b47acca
CFCode-git <435184766@qq.com>
2019-12-18 17:55:21 +0800

    准备加载图

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import x from './x.js'
-console.log('x')
-console.log('hi')
+import png from './assets/1.png'
+
+const div = document.getElementById('app')
+
+div.innerHTML = `
+    <img src ="./assets/1.png">
+`
