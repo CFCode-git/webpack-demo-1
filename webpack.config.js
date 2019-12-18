adf7c66cada0bcb853d7424061e6cfb0194b20e2
CFCode-git <435184766@qq.com>
2019-12-18 16:20:03 +0800

    准备安装webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,33 @@
-var path = require('path');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
     entry: './src/index.js',
     output: {
         filename: 'index.[contenthash].js'
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "XDML - 写代码啦",
+            template: "src/assets/template.html"
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
 };
+
+
+
+
+
+
+
+
+
